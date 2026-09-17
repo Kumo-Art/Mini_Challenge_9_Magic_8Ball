dafb29a389f2c875495567460cf8f9116bc84473
Kumo-Art <brandonlangehennig67@gmail.com>
2026-09-17 12:05:22 -0800

    mini challenge 9 update 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,78 @@
-﻿GameMechanics.GameGreeter();
+﻿//Brandon Langehennig
+//Mini Challenge 9
+//Peer Reviewer Name:
+//Review:
 
 
-string greetUser = Console.ReadLine();
+
+
+
+
+
+
+
+GameMechanics.GameGreeter();
+
+
+string greetUser = "yes";
+
+string userInput1 = Console.ReadLine();
+
+
+
+
+do
+{
+    
+Console.WriteLine("Great ask me a question!");
+    string userInput2 = Console.ReadLine();
+
+
+
+
+ string[] responses = {"Not likely." ,"Ask me later.","Yes." , "definitely.", "Maybe.", "Doubtful.", "Thinking......", "No.", "Depends."};
+
+
+Random rnd = new Random();
+
+int randomAnswer = rnd.Next(responses.Length);
+
+string rndAnswer = responses[randomAnswer];
+
+
+Console.WriteLine(rndAnswer);
+
+
+
+Console.WriteLine("Would you like to play again?");
+
+
+string playAgain = Console.ReadLine();
+
+while(playAgain == "yes")
+    {
+        Console.WriteLine("Great ask me another question?");
+    }
+if (playAgain == "no")
+    {
+        break;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}while(greetUser == userInput1);
+
+if(greetUser != userInput1);
+{
+    Console.WriteLine("No worries, have a nice day.");
+}
